377f0efec5f2ebd3c2814b59fd054ac9ac64ec3b
haliljon <110017001+haliljon@users.noreply.github.com>
2022-12-13 17:24:54 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,62 +19,7 @@ const list = document.querySelector('#list');
 const addNew = document.querySelector('#addNew');
 const contactLink = document.querySelector('#contact');
 const addBook = document.querySelector('#addBook');
-// class BookList {
-//   information = JSON.parse(localStorage.getItem('books')) || [];
 
-// storeBook() {
-//   const newBook = {
-//     name: title.value,
-//     author: author.value,
-//   };
-//   this.information.push(newBook);
-//   localStorage.setItem('books', JSON.stringify(this.information));
-//   return newBook;
-// }
-
-// displayBooks() {
-//   this.information.forEach((book) => {
-//     const bookTitle = document.createElement('p');
-//     const remove = document.createElement('button');
-//     remove.textContent = 'Remove';
-//     remove.type = 'button';
-//     remove.classList.add('remove');
-//     const bookDiv = document.createElement('div');
-//     bookDiv.classList.add('book-div');
-//     bookTitle.innerHTML = `${book.name} by ${book.author}`;
-//     bookDiv.append(bookTitle, remove);
-//     bookDisplayContainer.appendChild(bookDiv);
-//   });
-// }
-
-// removeBook(button, index) {
-//   bookDisplayContainer.removeChild(button.parentElement);
-//   this.information.splice(index, 1);
-//   localStorage.setItem('books', JSON.stringify(this.information));
-//   location.reload();
-// }
-
-// static showList() {
-//   awesomeBooks.style.display = 'block';
-//   contact.style.display = 'none';
-//   addBook.style.display = 'none';
-// }
-
-// static showNew() {
-//   awesomeBooks.style.display = 'none';
-//   contact.style.display = 'none';
-//   addBook.style.display = 'block';
-// }
-
-// static showContact() {
-//   awesomeBooks.style.display = 'none';
-//   contact.style.display = 'block';
-//   addBook.style.display = 'none';
-// }
-// }
-
-// const bookCollection = new BookList();
-// bookCollection.displayBooks();
 displayBooks.displayBook();
 addBook.style.display = 'block';
 const remove = document.querySelectorAll('.remove');
